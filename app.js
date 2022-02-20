6c98b52a696ecb81145353c2fedef48fd0348fa2
BAKHTIYOR SULAYMANOV <sulaymanof@gmail.com>
2022-02-20 09:30:06 -0500

    passenger counter app in JS (first commit)

---
+++ app.js
@@ -0,0 +1,138 @@
+// document.getElementById("count-el").innerText = 5;
+
+//let count = 0;
+
+// console.log(count);
+
+// 1. Create a variable, myAge, and set its value to your age
+// 2. Log the myAge variable to the console
+
+//let myAge = 39;
+//console.log(myAge);
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+// let myAge = 39;
+//let humanDogRatio = 7;
+//let myDogAge = myAge * humanDogRatio;
+//console.log(myDogAge);
+
+// 1. Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// 2. Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// function increment() {
+//   console.log("The button was clicked");
+// }
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function number() {
+//   console.log(42);
+// }
+// number();
+
+// let lap1 = 34;
+
+// let lap2 = 33;
+
+// let lap3 = 36;
+
+// // Create a function that logs out the sum of all lap times
+
+// function lapsLogger() {
+//   let sum = lap1 + lap2 + lap3;
+//   console.log(sum);
+// }
+// lapsLogger();
+
+// let lapsCompleted = 0;
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+// function lapsIncrement() {
+//   lapsCompleted = lapsCompleted + 1;
+//   console.log(lapsCompleted);
+// }
+
+// lapsIncrement();
+
+// lapsIncrement();
+
+// lapsIncrement();
+
+// document.getElementById("count").innerText = 5;
+
+// let count = 5;
+// count = count + 1;
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the HTML to reflect the new count
+
+// let count = 0;
+
+// let countEl = document.getElementById("count-el");
+// console.log(countEl);
+
+// function increment() {
+//   count = count + 1;
+//   countEl.innerText = count;
+//   // console.log(count);
+// }
+
+//increment();
+
+//
+
+// let username = "per";
+// let message = "You have tree new notifications";
+// console.log(message + ", " + username + "!");
+
+// // Create a variable, messageToUser, that contains the message we have logged
+
+// let messageToUser = message + ", " + username + "!";
+// console.log(messageToUser);
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+let myName = "Bakhtiyor";
+
+let greeting = "Hi, my name is ";
+
+let myGreeting = greeting + myName;
+console.log(myGreeting);
